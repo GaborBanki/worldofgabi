eb889a3dd6e891fe2933935c3d157fe9aac1442d
szekeres.gabor.2022i <szekeres.gabor.2022i@bankitatabanya.hu>
2025-10-15 13:07:04 +0200

    play and display methods, game loop with scanner

--- src/Game.java
+++ src/Game.java
@@ -1,5 +1,35 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Game {
+
     public static void main(String[] args) {
 
+        Game game = new Game();
+        game.play();
+    }
+
+    private void play() {
+        Scanner scanner = new Scanner(System.in);
+        display("Üdv a várkalandban");
+
+        // ez maga a játék ciklus
+        while (true) {
+
+            String input = scanner.nextLine().toLowerCase(Locale.ROOT).trim();
+            String command = input;
+
+            switch (command) {
+                case "kilép":
+                    display("Köszi a játékot!");
+                    scanner.close();
+                    return;
+            }
+
+        }
+    }
+
+    private void display(String message) {
+        System.out.println(message);
     }
 }
